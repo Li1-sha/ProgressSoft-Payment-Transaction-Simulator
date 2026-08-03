ea98b7209c724461197b2befd2b2ba1631ea4736
Li1-sha <shahadthy15@gmail.com>
2026-08-03 15:37:28 +0400

    pom.xml written with shade-plugin

--- intern-day2/src/main/java/com/progresssoft/GuessingGame.java
+++ intern-day2/src/main/java/com/progresssoft/GuessingGame.java
@@ -1,16 +1,11 @@
 package com.progresssoft;
 
-import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class GuessingGame {
     private final int secretNum;
     private final Scanner scanner;
-
-    public int getSecretNum() {
-        return secretNum;
-    }
 
     public GuessingGame(){
         Random random = new Random();
@@ -20,35 +15,13 @@ public class GuessingGame {
     }
     public void startGame(){
         int userNum = 0;
-        int attempts = 0;
         System.out.println("** Number Guessing game **");
-        System.out.println("Guess the number (1-100): ");
 
-        while (userNum != secretNum) {
-            try {
-                userNum = scanner.nextInt();
-                attempts++;
-
-                if (userNum < 1 || userNum > 100) {
-                    System.out.println("Please enter a number between 1 and 100!");
-                    continue;
-                }
-
-                if (userNum > secretNum) {
-                    System.out.println("Too high! Try again: ");
-                } else if (userNum < secretNum) {
-                    System.out.println("Too low! Try again: ");
-                } else {
-                    System.out.println("Correct! You got it in " + attempts + " attempts!");
-                }
-            } catch (InputMismatchException e) {
-                System.err.println("Error: Please enter a valid number!");
-                scanner.nextLine(); // Clear invalid input
-                System.out.println("Guess the number (1-100): ");
-            } catch (Exception e) {
-                System.err.println("Unexpected error: " + e.getMessage());
-                scanner.nextLine();
-                System.out.println("Guess the number (1-100): ");
+        while (userNum != secretNum){
+            userNum = scanner.nextInt();
+            if (userNum > secretNum) {
+            }else if (userNum < secretNum){
+            }else{
             }
         }
         scanner.close();
--- intern-day2/src/main/java/com/progresssoft/Main.java
+++ intern-day2/src/main/java/com/progresssoft/Main.java
@@ -7,7 +7,6 @@ public class Main {
          //GuessingGame game = new GuessingGame();
          //game.startGame();
          GradeTracker tracker = new GradeTracker();
-         tracker.enterStud((Student) null);
          System.out.println("Class average: "+tracker.calcAvg());
          System.out.println("Top student: "+tracker.findTopStudent());
 
