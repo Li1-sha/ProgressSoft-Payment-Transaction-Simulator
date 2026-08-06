47ee68746093c53421e797d0c4dee92a58caed87
Li1-sha <shahadthy15@gmail.com>
2026-08-06 14:14:59 +0400

    test: #4 add Mock & Spy variables

--- intern-day4/src/test/java/com/tdd/OrderServiceTest.java
+++ intern-day4/src/test/java/com/tdd/OrderServiceTest.java
@@ -1,4 +1,17 @@
 package com.tdd;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class OrderServiceTest {
+
+    @Mock
+    private PaymentGateway mockGateway;          // fully controlled fake
+
+    @Spy
+    private SimplePaymentGateway spyGateway;     // real code, but we can watch it
 }
