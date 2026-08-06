1ad49432e7d724e6edd542d47aed4abbd398426f
Li1-sha <shahadthy15@gmail.com>
2026-08-06 14:17:59 +0400

    test: #4 add mock place order test

--- intern-day4/src/test/java/com/tdd/OrderServiceTest.java
+++ intern-day4/src/test/java/com/tdd/OrderServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderServiceTest {
@@ -14,4 +16,15 @@ public class OrderServiceTest {
 
     @Spy
     private SimplePaymentGateway spyGateway;     // real code, but we can watch it
+
+    @Test
+    void testPlaceOrder_gatewayApproves_shouldReturnTrue() {
+        when(mockGateway.charge(anyDouble())).thenReturn(true);
+        OrderService service = new OrderService(mockGateway);
+
+        boolean result = service.placeOrder(100.0);
+
+        assertTrue(result);
+        verify(mockGateway, times(1)).charge(100.0);
+    }
 }
