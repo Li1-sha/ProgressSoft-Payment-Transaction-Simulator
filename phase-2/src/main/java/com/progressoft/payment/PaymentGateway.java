fb43b3bd0be685917a6f91595184e200034a4114
Li1-sha <shahadthy15@gmail.com>
2026-08-10 09:02:18 +0400

    refactor: #7 throws exceptions

--- phase-2/src/main/java/com/progressoft/payment/PaymentGateway.java
+++ phase-2/src/main/java/com/progressoft/payment/PaymentGateway.java
@@ -1,7 +1,9 @@
 package com.progressoft.payment;
 
 import com.progressoft.domain.Order;
+import com.progressoft.exception.GatewayTimeoutException;
+import com.progressoft.exception.InsufficientFundsException;
 
 public interface PaymentGateway {
-    void charge(Order order);
+    void charge(Order order) throws InsufficientFundsException, GatewayTimeoutException;;
 }
