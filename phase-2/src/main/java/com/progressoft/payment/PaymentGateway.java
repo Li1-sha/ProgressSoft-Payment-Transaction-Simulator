b838d04bb03fe0a8c3c9736aca6df42f7b1f4b5c
Li1-sha <shahadthy15@gmail.com>
2026-08-10 15:36:52 +0400

    fix: #7 gatewaytimeout undeclared

--- phase-2/src/main/java/com/progressoft/payment/PaymentGateway.java
+++ phase-2/src/main/java/com/progressoft/payment/PaymentGateway.java
@@ -1,9 +1,8 @@
 package com.progressoft.payment;
 
 import com.progressoft.domain.Order;
-import com.progressoft.exception.GatewayTimeoutException;
 import com.progressoft.exception.InsufficientFundsException;
 
 public interface PaymentGateway {
-    void charge(Order order) throws InsufficientFundsException, GatewayTimeoutException;
+    void charge(Order order) throws InsufficientFundsException;
 }
